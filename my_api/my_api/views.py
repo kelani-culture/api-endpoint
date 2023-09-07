6536bd7dd051c48b65e8d34b8759fc07d966117e
Null <kelanidarasimi9@gmail.com>
2023-09-07 23:47:03 +0100

    rewrote the code to meet the desired output

--- my_api/my_api/views.py
+++ my_api/my_api/views.py
@@ -7,24 +7,24 @@ def welcome_view(request):
     return HttpResponse("Welcome")
 
 
-def user_info(request):
+def user_info(request) -> JsonResponse:
     """
         Return User request in Json response
     """
     slack_name = request.GET.get('slack_name')
-    track_name = request.GET.get('track')
+    track = request.GET.get('track')
 
-    if (slack_name and track_name):
-        try:
-            slack_name = UserInfo.objects.get(slack_name=slack_name)
-            track_name = UserInfo.objects.get(track=track_name)
-
-        except UserInfo.DoesNotExist:
-            return HttpResponseBadRequest("Invalid parameters")
-
-        else:
-            info = UserInfo.objects.all()
-            serializer = UserInfoSerializer(info, many=True)
-            return JsonResponse(serializer.data, safe=False)
+    if slack_name and track:
+        user_info = UserInfo.objects.all()
+        user_data = {
+            "slack": slack_name,
+            "current_day":user_info[0].current_day,
+            "utc_time": user_info[0].utc_time,
+            "track": track,
+            "github_file_url": user_info[0].github_file_url,
+            "github_repo_url": user_info[0].github_repo_url,
+            "status_code": user_info[0].status_code
+        }
+        return JsonResponse(user_data, safe=False)
     else:
-        return HttpResponseBadRequest("Missing parameter")
+        return JsonResponse({"error": None}, safe=False)
